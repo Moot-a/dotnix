8e701972af027a2e8ae4d93473d0e774511df99a
Moota <moota.moota@proton.me>
2025-05-01 11:45:56 +0200

    Selected AMD GPU + CPU Hardware

--- configuration.nix
+++ configuration.nix
@@ -32,26 +32,11 @@ in
 
     # === GPU-specific configurations ===
 
-    /*
-      For drivers, we are leveraging nixos-hardware
-      Most common drivers are below, but you can see more options here: https://github.com/NixOS/nixos-hardware
-    */
-
-    #! EDIT THIS SECTION
-    # For NVIDIA setups
-    # inputs.hydenix.inputs.nixos-hardware.nixosModules.common-gpu-nvidia
-
-    # For AMD setups
-    # inputs.hydenix.inputs.nixos-hardware.nixosModules.common-gpu-amd
-
-    # === CPU-specific configurations ===
-    # For AMD CPUs
-    # inputs.hydenix.inputs.nixos-hardware.nixosModules.common-cpu-amd
-
-    # For Intel CPUs
-    inputs.hydenix.inputs.nixos-hardware.nixosModules.common-cpu-intel
-
-    # === Other common modules ===
+    /* https://github.com/NixOS/nixos-hardware */
+    
+    inputs.hydenix.inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.hydenix.inputs.nixos-hardware.nixosModules.common-cpu-amd
+    
     inputs.hydenix.inputs.nixos-hardware.nixosModules.common-pc
     inputs.hydenix.inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
