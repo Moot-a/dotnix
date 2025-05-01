2dd975b241a83f5481e8a68a26dd7c1c7b0310ea
Moota <moota.moota@proton.me>
2025-05-01 17:48:27 +0200

    updated setup to include hyprland

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 # sudo ln -s ~/dotnix/nixos /etc/nixos <- ~ expands to root user directory not personal
 sudo mv /etc/nixos /etc/nixos.bak
 sudo ln -s /home/moota/dotnix/nixos /etc/nixos # <- Change moota with your usename
-ln -s ~/dotnix/home ~/.config/home-manager
+ln -s /home/moota/dotnix/home ~/.config/home-manager
+# Remove folder before symlinking
+ln -s /home/moota/dotnix/hypr ~/.config/hypr
